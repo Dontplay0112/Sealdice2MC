9eda2968fec34b01aee83c4b2cc1129e6826a9bc
dontplay1003 <dontplay1003@gmail.com>
2026-04-03 03:28:40 +0800

    fix file missing

--- forge/src/main/java/top/dontplay/forge/SealDice2MCForge.java
+++ forge/src/main/java/top/dontplay/forge/SealDice2MCForge.java
@@ -1,12 +1,15 @@
 package top.dontplay.forge;
 
-import net.forge.fml.common.Mod;
+import dev.architectury.platform.forge.EventBuses;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import top.dontplay.SealDice2MC;
 
 @Mod(SealDice2MC.MOD_ID)
 public final class SealDice2MCForge {
     public SealDice2MCForge() {
+        EventBuses.registerModEventBus(SealDice2MC.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         SealDice2MC.init();
     }
 }

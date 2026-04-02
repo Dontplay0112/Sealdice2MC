ad69481cda168403645e8f66172a0223c28a3217
dontplay1003 <dontplay1003@gmail.com>
2026-04-03 03:14:03 +0800

    init to 1.20.1

---
+++ forge/src/main/java/top/dontplay/forge/SealDice2MCForge.java
@@ -0,0 +1,12 @@
+package top.dontplay.forge;
+
+import net.forge.fml.common.Mod;
+
+import top.dontplay.SealDice2MC;
+
+@Mod(SealDice2MC.MOD_ID)
+public final class SealDice2MCForge {
+    public SealDice2MCForge() {
+        SealDice2MC.init();
+    }
+}
